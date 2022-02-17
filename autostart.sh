77b2ac058f90f094df6988585b23e802457f27c8
Gustavo Rangel <gustavorangel91@gmail.com>
2022-02-17 12:07:22 -0300

    Comment out dunst.

--- autostart.sh
+++ autostart.sh
@@ -9,7 +9,7 @@ function run {
 feh --no-fehbg --bg-fill --randomize $HOME/drive/wallpapers/* &
 run sxhkd -c ~/.config/misc/sxhkdrc &
 sh $HOME/scripts/remaps.sh &
-dunst &
+# dunst &
 picom --config $HOME/.config/misc/picom.conf &
 run telegram-desktop &
 run whatsapp-nativefier &
