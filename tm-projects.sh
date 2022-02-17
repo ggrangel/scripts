03f5e3dedf0ed4c8190bc9b1a9af8699f884b12a
Gustavo Rangel <gustavorangel91@gmail.com>
2022-02-17 18:26:31 -0300

    Cd to folder before open it with vim.

--- tm-projects.sh
+++ tm-projects.sh
@@ -4,9 +4,9 @@ if ! tmux has-session -t newsweek; then
 	tmux new -s newsweek -n "dev" -d
 	tmux new-window -n "dotfiles" -t newsweek:2
 	tmux new-window -n "images" -t newsweek:3
-	tmux send-keys -t newsweek:1 "v ~/projects/altcademy/html-css/newsweek/newsweek/index.html" Enter
-	tmux send-keys -t newsweek:2 "v ~/.config/" Enter
-	tmux send-keys -t newsweek:3 "ranger ~/projects/altcademy/html-css/newsweek/newsweek/images/" Enter
-    tmux select-window -t newsweek:1
-    tmux attach-session
+	tmux send-keys -t newsweek:1 "cd $HOME/projects/altcademy/html-css/newsweek/newsweek/ && v index.html" Enter
+	tmux send-keys -t newsweek:2 "cd $HOME/.config/ && v ." Enter
+	tmux send-keys -t newsweek:3 "cd $HOME/projects/altcademy/html-css/newsweek/newsweek/images/ && rangercd" Enter
+	tmux select-window -t newsweek:1
+	tmux attach-session
 fi
