e7fde9dda3b72c39803462bebb31638c33f29fd7
Gustavo Rangel <gustavorangel91@gmail.com>
2022-02-17 11:13:50 -0300

    Add notification message.

--- screenshooter.sh
+++ screenshooter.sh
@@ -14,12 +14,15 @@ imagePath="$defaultFolder/$name".png
 case $1 in
 'screen')
 	maim "$imagePath"
+    notify-send 'Screenshooter' "Screen print saved to $imagePath"
 	;;
 'window')
     maim -i "$(xdotool getactivewindow)" "$imagePath"
+    notify-send 'Screenshooter' "Window print saved to $imagePath"
 	;;
 'select')
 	maim -s "$imagePath"
+    dunstify 'Screenshooter' "Region print saved to $imagePath"
 	;;
 *)
 	exit 0
