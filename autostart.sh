49e1b52e78fb873c675d4139a39dd1f9f53fc3af
Gustavo Rangel <gustavorangel91@gmail.com>
2022-02-17 11:17:35 -0300

    Run telegram and whatsapp at startup.

--- autostart.sh
+++ autostart.sh
@@ -7,18 +7,13 @@ function run {
 }
 
 feh --no-fehbg --bg-fill --randomize $HOME/drive/wallpapers/* &
-
 run sxhkd -c ~/.config/misc/sxhkdrc &
-
-#starting utility applications at boot time
-numlockx &
+sh $HOME/scripts/remaps.sh &
+dunst &
 picom --config $HOME/.config/misc/picom.conf &
-xautolock -time 30 -locker slock &
-sh $HOME/scripts/remaps.sh
+run telegram-desktop &
+run whatsapp-nativefier &
 insync start &
+numlockx &
 lxsession &
-# /usr/lib/xfce4/notifyd/xfce4-notifyd &
-
-# run xfce4-power-manager &
-#starting user applications at boot time
-# run telegram-desktop &
+xautolock -time 30 -locker slock &
