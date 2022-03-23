fc3466a0830080de524a7034175403d29668102a
Gustavo Rangel <gustavorangel91@gmail.com>
2022-03-23 16:56:22 -0300

    Create gh-repos.

---
+++ gh-repos.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check status of my github repos
+
+REPOS=( 
+"/home/rangelgbr/.password-store/"
+"/home/rangelgbr/.config/"
+"/home/rangelgbr/scripts/"
+)
+
+for repo in "${REPOS[@]}"; do
+    printf "\nRepo: %s\n" "$repo"
+    cd "$repo" && git status
+done
