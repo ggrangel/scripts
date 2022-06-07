0e34db5a2b7a209eec6d9f8dd1e005022e9e3139
Gustavo Rangel <gustavorangel91@gmail.com>
2022-06-07 07:59:35 -0300

    Improve date format.

--- screenshooter.sh
+++ screenshooter.sh
@@ -16,28 +16,26 @@
 # xclip
 
 defaultFolder="$HOME/tmp/screenshots/"
-defaultName=$(date +%s)
+defaultName=$(date "+%d-%m-%y-%H_%M_%S")
 
 mkdir -p "$defaultFolder"
 
-imagePath="$defaultFolder/$defaultName".png
+imagePath="$defaultFolder$defaultName.png"
 
 case $1 in
 'screen')
 	maim "$imagePath"
-	notify-send 'Screenshooter' "Screen print saved to $imagePath"
 	;;
 'window')
 	maim -i "$(xdotool getactivewindow)" "$imagePath"
-	notify-send 'Screenshooter' "Window print saved to $imagePath"
 	;;
 'select')
 	maim -s "$imagePath"
-	notify-send 'Screenshooter' "Region print saved to $imagePath"
 	;;
 *)
 	exit 0
 	;;
 esac
 
+notify-send 'Screenshooter' "Screenshot saved to $imagePath"
 xclip -selection clipboard -t image/png -i "$imagePath"
