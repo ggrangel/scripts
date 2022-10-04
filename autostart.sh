1c7ce7d0bdc29109aae3100fe49b26a716b4e04f
Gustavo Rangel <gustavorangel91@gmail.com>
2022-10-04 15:38:30 -0300

    remove run function

--- autostart.sh
+++ autostart.sh
@@ -15,24 +15,18 @@
 # xautolock
 # volnoti
 
-function run {
-	if ! pgrep $(basename $2); then
-		$@ &
-	fi
-}
-
 sh ~/scripts/remaps.sh &
 sxhkd -c ~/.config/sxhkd/sxhkdrc &
 picom --config ~/.config/picom.conf &
 redshift -P -O 5000
+python ~/scripts/wallpaper.py set &
 insync start &
 xautolock -time 60 -locker "systemctl suspend"
 volnoti &
-python ~/scripts/wallpaper.py set &
 
 hostName=$(cat /etc/hostname)
 if [[ $hostName == "core" ]]; then
-	sxhkd -c ~/.config/sxhkd/sxhkdrc.core &
+    sxhkd -c ~/.config/sxhkd/sxhkdrc.core &
 elif [[ $hostName == "aux" ]]; then
-	sxhkd -c ~/.config/sxhkd/sxhkdrc.aux &
+    sxhkd -c ~/.config/sxhkd/sxhkdrc.aux &
 fi
